88476cfe5d60073cd599ccdf90a71a3ab7981a4e
Remco Brink <remco@rc6.org>
2014-09-05 23:01:57 +0200

    Save to radiostations.js

--- raspberry_pi/difm.sh
+++ raspberry_pi/difm.sh
@@ -22,17 +22,45 @@ if [ "$prefix" == "" ]; then
   prefix="difm"
 fi
 
+if [ "$listenkey" == "" ]; then
+  echo "Using public playlists"
+else
+  echo "Using premium playlists"
+fi
+
 # List of DI.FM channels
 url="http://listen.di.fm/public3"
 
-for name in `wget -nv -O - $url | grep -o '"key":"[^"]*"' | sed 's/"key":"\([^"]*\)"/\1/g'`; do
-echo $name
+for name in `wget -q -nv -O - $url | grep -o '"key":"[^"]*"' | sed 's/"key":"\([^"]*\)"/\1/g'`; do
+echo "- Fetching $name"
   file="$dir/$prefix-$name.m3u"
   if [ "$listenkey" == "" ]; then
-    wget -nv -O $file "http://listen.di.fm/public3/$name.pls"
+    wget -q -nv -O $file "http://listen.di.fm/public3/$name.pls"
   else
-    wget -nv -O $file "http://listen.di.fm/premium_high/$name.pls?$listenkey"
+    wget -q -nv -O $file "http://listen.di.fm/premium_high/$name.pls?$listenkey"
   fi
   sed -n -i 's/^File[0-9]*=//p' $file
+done
+
+# Create new radiostations.js file
+cat <<EOF > radiostations.js
+/**
+ * Default Radiostations which appear in the webinterface. Edit if you like.
+ * Take care when editting. Only edit the stuff between ''
+ * And don't use the default Windows Notepad for this (use Notepad++ on Windows)
+ */
+
+var radioStations = [];
+EOF
+
+PLAYLISTFILES="$dir/*.m3u"
+for f in $PLAYLISTFILES
+do
+  STREAMURL=$(head -1 $f)
+  filename=$(basename "$f")
+  extension="${filename##*.}"
+  filename="${filename%.*}"
+  filename=${filename#$prefix-}
+  echo "radioStations.push(['$filename', '$STREAMURL']);" >> radiostations.js
 done
 
